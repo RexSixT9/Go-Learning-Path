93de0a02ceadb046db8a046ac0fce0543d025b5c
Amal Raj <amalrajkvdev@gmail.com>
2026-06-24 19:58:51 +0530

    renamed a dir

--- 21_goroutines/main.go
+++ 21_goroutines/main.go
@@ -1,19 +1,56 @@
-// This program demonstrates the use of goroutines in Go. It starts multiple tasks concurrently using goroutines. Each task prints its ID to the console. However, since the main function does not wait for the goroutines to finish, the program may exit before all tasks have completed. To ensure that all tasks complete before the program exits, you can use synchronization techniques such as WaitGroups or channels. In this example, we simply start the goroutines without waiting for them, which may lead to some tasks not being printed if the main function exits too quickly.
+// Goroutines are lightweight threads managed by the Go runtime. They allow concurrent execution of functions, making it easier to write concurrent programs. In this example, we will demonstrate how to create and run goroutines in Go. Go provides the `go` keyword to start a new goroutine. When you call a function with the `go` keyword, it runs concurrently with the calling function.
+
+// package main
+
+// import (
+// 	"fmt"
+// 	// "time"
+// )
+
+// func task(id int) {
+// 	fmt.Printf("Task %d is running\n", id)
+// }
+
+// func main() {
+// 	for i := 1; i <= 5; i++ {
+// 		go task(i) // Start a new goroutine for each task
+// 	}
+// 	// time.Sleep(1 * time.Second) // Sleep for a second to allow goroutines to finish (not a recommended practice)
+// }
+
+// Go Concurrency vs Parallelism := Concurrency is the ability of a program to manage multiple tasks at the same time, while parallelism is the ability to execute multiple tasks simultaneously. In Go, goroutines provide concurrency, allowing you to write programs that can handle multiple tasks concurrently without blocking the main execution flow.
 
 package main
 
 import (
 	"fmt"
-	// "time"
+	"time"
 )
 
-func task(id int) {
-	fmt.Printf("Task %d is running\n", id)
-}
-
 func main() {
-	for i := 1; i <= 5; i++ {
-		go task(i) // Start a new goroutine for each task
-	}
-	// time.Sleep(1 * time.Second) // Sleep for a second to allow goroutines to finish (not a recommended practice)
+
+	start := time.Now()
+
+	go func() {
+		time.Sleep(300 * time.Millisecond)
+		fmt.Println("Goroutine 1 finished", time.Since(start))
+	}()
+
+	go func() {
+		time.Sleep(150 * time.Millisecond)
+		fmt.Println("Goroutine 2 finished", time.Since(start))
+	}()
+
+	fmt.Println("Main function finished", time.Since(start))
+
+	fmt.Println("Waiting...", time.Since(start))
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Println("Waiting...", time.Since(start))
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Println("Waiting...", time.Since(start))
+	time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("All goroutines finished", time.Since(start))
 }
